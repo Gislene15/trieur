4590965d9c6e865d3b03d043ee0ef5d7759dc572
Gislene <gisleneneves16@gmail.com>
2024-12-11 17:24:15 +0100

    test bouton poussoir et pot

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,40 @@
 #include <Arduino.h>
 #include "rgb_lcd.h"
-
+#define RED 1
 rgb_lcd lcd;
-
-void setup() {
+int BP1 = 2, BP2 = 12, BP0 = 0;
+int val_BP1, val_BP2, val_BP0;
+int pot = 33;
+int lecture_pot;
+void setup()
+{
   // Initialise la liaison avec le terminal
   Serial.begin(115200);
 
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
-  lcd.printf("Trieur de balles");
+  // lcd.printf("Gigi la GOAT");
+  lcd.setColor(2);
 
+  pinMode(BP0, INPUT_PULLUP);
+  pinMode(BP1, INPUT_PULLUP);
+  pinMode(BP2, INPUT_PULLUP);
+
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  val_BP0 = digitalRead(BP0);
+  val_BP1 = digitalRead(BP1);
+  val_BP2 = digitalRead(BP2);
+  lcd.setCursor(0, 0);
+  // lcd.printf("VAL BP2%D\n",val_BP2);
+  lcd.printf("BP0%d BP1%d BP2%d",val_BP0,val_BP1, val_BP2);
+
+  lecture_pot=analogRead(pot);
+  lcd.setCursor(0, 1);
+  lcd.printf("pot=%d",lecture_pot);
 
 }
