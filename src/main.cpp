8e18a4071297c4ef027670d1335621637a28a6de
Gislene <gisleneneves16@gmail.com>
2024-12-11 17:45:56 +0100

    pwm

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@ int BP1 = 2, BP2 = 12, BP0 = 0;
 int val_BP1, val_BP2, val_BP0;
 int pot = 33;
 int lecture_pot;
+
+//PWM
+int pwm1 = 27;
+int frequence = 500;
+int canal0 = 0;
+int canal1 = 2;
+int resolution = 10;
+
 void setup()
 {
   // Initialise la liaison avec le terminal
@@ -16,11 +24,13 @@ void setup()
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
   // lcd.printf("Gigi la GOAT");
+  // changer couleur sur le LCD
   lcd.setColor(2);
-
+// configuration des boutons
   pinMode(BP0, INPUT_PULLUP);
   pinMode(BP1, INPUT_PULLUP);
   pinMode(BP2, INPUT_PULLUP);
+
 
   Serial.begin(9600);
 }
