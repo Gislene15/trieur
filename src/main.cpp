7a56f00e4c70e3cd54055b00d2aea9e09a392be3
gislene <gisleneneves16@gmail.com>
2024-12-12 17:54:57 +0100

    horaire

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,16 @@
 rgb_lcd lcd;
 int BP1 = 2, BP2 = 12, BP0 = 0;
 int val_BP1, val_BP2, val_BP0;
+
 int pot = 33;
 int lecture_pot;
 
-//PWM
+// PWM
 int pwm1 = 27;
-int frequence = 500;
+int frequence = 25000;
 int canal0 = 0;
-int canal1 = 2;
-int resolution = 10;
+int resolution = 11;
+int non_horraire = 26;
 
 void setup()
 {
@@ -26,11 +27,17 @@ void setup()
   // lcd.printf("Gigi la GOAT");
   // changer couleur sur le LCD
   lcd.setColor(2);
-// configuration des boutons
+
+  // configuration des boutons
   pinMode(BP0, INPUT_PULLUP);
   pinMode(BP1, INPUT_PULLUP);
   pinMode(BP2, INPUT_PULLUP);
+  pinMode(non_horraire, OUTPUT);
 
+  // configuration de la PWM
+  ledcSetup(canal0, frequence, resolution);
+  // liison des canux des PWM avec les broches de l'ESP32
+  ledcAttachPin(pwm1, canal0);
 
   Serial.begin(9600);
 }
@@ -40,12 +47,38 @@ void loop()
   val_BP0 = digitalRead(BP0);
   val_BP1 = digitalRead(BP1);
   val_BP2 = digitalRead(BP2);
+
+  // non_horraire = digitalRead(non_horraire);
+
+  /*if (val_BP0==LOW)
+  {
+  digitalWrite(non_horraire,LOW);
+  ledcWrite(canal0, 511 );
+  lcd.setCursor(0, 0);
+  lcd.printf(" sens horaire ");
+  }
+  else if(val_BP1==LOW)
+  {
+  digitalWrite(non_horraire,HIGH);
+  ledcWrite(canal0, 511 );
+  lcd.setCursor(0, 1);
+  lcd.printf("sens non horaire");
+  }
+  else{
+  lcd.setCursor(0, 1);
+  lcd.printf("stop");
+  }
+  */
+
+  digitalWrite(non_horraire, LOW);
+  ledcWrite(canal0, 511);
+
   lcd.setCursor(0, 0);
   // lcd.printf("VAL BP2%D\n",val_BP2);
-  lcd.printf("BP0%d BP1%d BP2%d",val_BP0,val_BP1, val_BP2);
+  lcd.printf("BP0%d BP1%d BP2%d", val_BP0, val_BP1, val_BP2);
 
-  lecture_pot=analogRead(pot);
+  lecture_pot = analogRead(pot);
+
   lcd.setCursor(0, 1);
-  lcd.printf("pot=%d",lecture_pot);
-
+  lcd.printf("pot=%d", lecture_pot);
 }
