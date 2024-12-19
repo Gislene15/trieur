8d0900cfb4a0764dbe40655f053d9e9644e8da0d
gislene <gisleneneves16@gmail.com>
2024-12-19 12:42:15 +0100

    servo moteur code

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ rgb_lcd lcd;
 int BP1 = 2, BP2 = 12, BP0 = 0;
 int val_BP1, val_BP2, val_BP0;
 
+// pot init
 int pot = 33;
 int lecture_pot;
 
@@ -31,6 +32,10 @@ int canal0 = 0;
 int resolution = 11;
 int non_horraire = 26;
 
+int servo = 13;
+int canal2 = 2;
+int frequence1= 50;
+int resolution1 =10;
 void setup()
 {
   Serial.begin(115200);
@@ -47,14 +52,20 @@ void setup()
   pinMode(BP0, INPUT_PULLUP);
   pinMode(BP1, INPUT_PULLUP);
   pinMode(BP2, INPUT_PULLUP);
+
   // configuration en sortie de la pin sgn qui permet de la mettre en non horaire
   pinMode(non_horraire, OUTPUT);
   pinMode(pwm1, OUTPUT);
-
   // configuration de la PWM
   ledcSetup(canal0, frequence, resolution);
   // liaison des canux des PWM avec les broches de l'ESP32
   ledcAttachPin(pwm1, canal0);
+
+  pinMode(servo, OUTPUT);
+  // configuration du servo
+  ledcSetup(canal2, frequence1, resolution);
+  // liaison des canux des servo avec les broches de l'ESP32
+  ledcAttachPin(servo, canal2);
 
   if (tcs.begin())
   {
@@ -74,64 +85,80 @@ void loop()
   val_BP0 = digitalRead(BP0);
   val_BP1 = digitalRead(BP1);
   val_BP2 = digitalRead(BP2);
-
-  lcd.setCursor(0, 0);
-  // lcd.printf("VAL BP2%D\n",val_BP2);
-  lcd.printf("BP0%d BP1%d BP2%d", val_BP0, val_BP1, val_BP2);
-
   // pot
   lecture_pot = analogRead(pot);
   lcd.setCursor(0, 1);
   lcd.printf("pot=%d", lecture_pot);
 
+  /*lcd.setCursor(0, 0);
+  // lcd.printf("VAL BP2%D\n",val_BP2);
+  lcd.printf("BP0%d BP1%d BP2%d", val_BP0, val_BP1, val_BP2);
+
+
   // pwm
-  if (lecture_pot > 2047)
+  if (lecture_pot > 100)
   {
-    lecture_pot = 2047;
-  }
-  if (val_BP0 == LOW)
-  {
-    digitalWrite(non_horraire, LOW);
-    ledcWrite(canal0, lecture_pot);
-    lcd.setCursor(0, 0);
-    lcd.printf(" sens horaire ");
-  }
-  else if (val_BP1 == LOW)
-  {
-    digitalWrite(non_horraire, HIGH);
-    ledcWrite(canal0, lecture_pot);
-    lcd.setCursor(0, 0);
-    lcd.printf("sens non horaire");
-  }
-  else if (val_BP2 == LOW)
-  {
-    ledcWrite(canal0, 0);
-    lcd.setCursor(0, 1);
-    lcd.printf("stop");
-  }
+    lecture_pot = 100;
+    }*/
+/*if (val_BP0 == LOW)
+{
+  digitalWrite(non_horraire, LOW);
+  ledcWrite(canal0, lecture_pot);
+  lcd.setCursor(0, 0);
+  lcd.printf(" sens horaire ");
+}
+else if (val_BP1 == LOW)
+{
+  digitalWrite(non_horraire, HIGH);
+  ledcWrite(canal0, lecture_pot);
+  lcd.setCursor(0, 0);
+  lcd.printf("sens non horaire");
+}
+else if (val_BP2 == LOW)
+{
+  ledcWrite(canal0, 0);
+  lcd.setCursor(0, 1);
+  lcd.printf("stop");
+}*/
 
-  // couleur
-  tcs.getRawData(&r, &g, &b, &c);
-  lux = tcs.calculateLux(r, g, b);
-  // colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+// couleur
+/*tcs.getRawData(&r, &g, &b, &c);
+lux = tcs.calculateLux(r, g, b);
+// colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
 
-  Serial.print("Color Temp: ");
-  Serial.print(colorTemp, DEC);
-  Serial.print(" K - ");
-  Serial.print("Lux: ");
-  Serial.print(lux, DEC);
-  Serial.print(" - ");
-  Serial.print("R: ");
-  Serial.print(r, DEC);
-  Serial.print(" ");
-  Serial.print("G: ");
-  Serial.print(g, DEC);
-  Serial.print(" ");
-  Serial.print("B: ");
-  Serial.print(b, DEC);
-  Serial.print(" ");
-  Serial.print("C: ");
-  Serial.print(c, DEC);
-  Serial.print("  \n");
-  delay(100);
+Serial.print("Color Temp: ");
+Serial.print(colorTemp, DEC);
+Serial.print(" K - ");
+Serial.print("Lux: ");
+Serial.print(lux, DEC);
+Serial.print(" - ");
+Serial.print("R: ");
+Serial.print(r, DEC);
+Serial.print(" ");
+Serial.print("G: ");
+Serial.print(g, DEC);
+Serial.print(" ");
+Serial.print("B: ");
+Serial.print(b, DEC);
+Serial.print(" ");
+Serial.print("C: ");
+Serial.print(c, DEC);
+Serial.print("  \n");
+delay(100);*/
+
+// servo
+if (val_BP0 == LOW)
+{
+  ledcWrite(canal2, 0);
+}
+/*else if( val_BP1==LOW)
+ {
+  ledcWrite(canal2,0);
+ }
+ else if( val_BP2==LOW)
+ {
+  ledcWrite(canal2,0);
+ }
+ */
+
 }
